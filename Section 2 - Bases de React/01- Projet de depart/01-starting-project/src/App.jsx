a4776c01b9b93e5e49fde7d6760e446c8d3313de
Emile Litalien <e2269456@cmaisonneuve.qc.ca>
2024-03-18 12:10:46 -0400

    Quizz 1 à Exercice 4
    Éléments dynamiques, meilleure façon de prendre des images (Avec import), mettre des éléments dynamiques dans du code statique

--- Section 2 - Bases de React/01- Projet de depart/01-starting-project/src/App.jsx
+++ Section 2 - Bases de React/01- Projet de depart/01-starting-project/src/App.jsx
@@ -1,13 +1,34 @@
+// * Commencement d'elements dynamiques 
 
+const reactDescriptions = ['Fundamental', ' Crucial', 'Core', 'Important', 'Useful'];
+
+function generationNombreRandom(max){
+    return Math.floor(Math.random()*(max+1));
+}
+// Dans l'element statique (Ici Header) {} pour mettre un element dynamique
+// 
+// * 
+
+// ** Un meilleur moyen de load des images 
+
+// Chargement d'une image. Le src = "src/PATH" n'est pas super car quand on deploie notre code React, l'image peut etre perdu (Image file ignored during bundling). 
+
+import reactImg from './assets/react-core-concepts.png';
+// Tjrs avec {}  dans code car element dynamique 
+// ** 
 
 // Mon premier composant
 function Header(){
+
+  // On peut aussi mettre le calcul directement dans les {} mais recommender comme ceci pour garder un code plus propre et comprehensible
+  const description = reactDescriptions[generationNombreRandom(4)];
+  
   return(
     <header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <img src={reactImg} alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
-      Fundamental React concepts you will need for almost any app you are
+      {description} React concepts you will need for almost any app you are
       going to build!
 
       
