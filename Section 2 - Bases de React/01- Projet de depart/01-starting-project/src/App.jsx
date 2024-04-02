530d1a4f2fea4992624df590b1ff474c637ab403
Emile Litalien <e2269456@cmaisonneuve.qc.ca>
2024-04-02 13:22:31 -0400

    Quizz 3 à Quizz 4
    Introduction des states et hooks (onState), on load de l'information dynamiquement selon les boutons appuyés

--- Section 2 - Bases de React/01- Projet de depart/01-starting-project/src/App.jsx
+++ Section 2 - Bases de React/01- Projet de depart/01-starting-project/src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header/Header.jsx';
 import ConceptImportant from './components/Conceptes/ConceptImp1.jsx';
 import ConceptImportantOptimiser from './components/Conceptes/ConceptImp2.jsx';
 import TabBoutons from './components/TABBoutons/TABBoutons.jsx';
+// Import pour infos qui change quand on select un bouton
+import { EXAMPLES } from './data-with-examples.js';
 
 // ** Un meilleur moyen de load des images 
 // Chargement d'une image. Le src = "src/PATH" n'est pas super car quand on deploie notre code React, l'image peut etre perdu (Image file ignored during bundling). 
@@ -17,17 +19,32 @@ import componentsImg from './assets/components.png';
 import {CORE_CONCEPTS} from './data.js';
 // *** 
 
-
+// **** Hooks et States
+// Everything that starts with 'use' is a Hook. Ex: useState
+import {useState} from 'react';
+// ****
 
 // In function App() theres those 
 // <TabBoutons onSelect={() => handleSelect('components')}> Components </TabBoutons>
 // The () => handleSelect('components') = Ghost function executed, how handleSelect is going to be called (it has () so its called from in the Ghost function) then the data its called with is 'components'. Its wrapped in another function so it wont get executed too early (Will only execute when activated) 
 function App() {
+  // Issue with having a let VARIABLE = Will execute it ONCE. 
+  // let tabContent = 'Cliquer un bouton';
+  //Need to use STATES and HOOKS
+  // Hooks must be called on the top level of the function. (Cant be nested). useState returns an arra w/ 2 elements (ALWAYS)
+  // let tableauState = useState('Appuyer sur un bouton'); 
+  // Ici je vais defaire l'array. Nommer selectedTopic et setSelectedTopic de par les conventions
+  // Infos entre () apres useState = Celle de depart, selectedTopic vient la replacer quand executer. 
+  // setSelectedTopic = Fonction qui update selectedTopic et dit a React de re-executer useState 
+  let [selectedTopic, setSelectedTopic ] = useState('components');
 
   function handleSelect(buttonSelected){
     // buttonSelected = The buttons (Components, JSX, Props, State) It'll be 1 of these 4 
     // Now buttonSelected = the value put in () => handleSelect(THIS VALUE)
-    console.log(buttonSelected);
+    setSelectedTopic(buttonSelected);
+    // UI montre la nouvelle valeur, mais le selectedTopic est toujours l'ancienne car React ne va updater selectedTopic que quand useState sera re-executer.
+    console.log(selectedTopic);
+
   }
 
   return (
@@ -147,7 +164,16 @@ function App() {
             <TabBoutons onSelect={() => handleSelect('props')}> Props </TabBoutons>
             <TabBoutons onSelect={() => handleSelect('state')}> state </TabBoutons>
           </menu>
-            Dynamic Content (Thing under buttons that change)
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
+
         </section>
       </main>
       
